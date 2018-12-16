272c90438ba3f6a3d06a7cceb69b4fe7791fbb06
Daniloc <daniloc@MacBook-Pro-de-Danilo.local>
2018-12-15 22:08:42 -0200

    inicio da resolução dos execs

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arrayAleatorio = [`Danilo`, 99, {animal:'cachorro'}, numeroLong = 1.89];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function myFunction (array){
+    return array;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(myFunction(arrayAleatorio)[1]); 
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,25 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function doisParam (arrayParam, numeberParam){
+    console.log(arrayParam[numeberParam]);
+}
+
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var arrayLocao = [true, 'Danilo', 1.89, NaN, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+doisParam// Chamada da Funcao
+doisParam(arrayLocao,3);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +58,8 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
